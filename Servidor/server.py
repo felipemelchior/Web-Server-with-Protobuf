7d270f7a06d5ef07285999570989b3b7862d4c73
lucas <lucas.lukinha67@gmail.com>
2018-10-27 10:17:54 -0300

    stop threads

--- Servidor/server.py
+++ Servidor/server.py
@@ -71,11 +71,17 @@ def listenConnection(Ip, Port):
 
 		logging.info(" WebServer running on port {0}".format(Port))
 
+		threads = []
+
 		try:
 			while True:
 				conn, addr = server.accept()
 				logging.info(" New Connection from " + str(addr[0]) + " with port " + str(addr[1]))
-				threading.Thread(target=connected, args=(conn,addr)).start()
+				
+				aux = threading.Thread(target=connected, args=(conn,addr))
+				aux.setDaemon(True)				
+				aux.start()
+				threads.append(aux)
 		except:
 			if(os.getuid() == 0):
 				clearRules()
